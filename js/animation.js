5f10f95802a9193846c84e12c51e00bfdc533f03
quang nguyen <quangcrazymen@gmail.com>
2024-06-09 23:21:34 +0700

    + Add animation based on name

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,70 @@
 // class Animation{
 
 // }
-function slideOutEllipticRightFwd() {}
+// LIST OF ANIMATION
+function slideOutEllipticRightFwd(container) {
+	container.animate(
+		[
+			{
+				offset: 0,
+				transform: "translateX(0) rotateY(0) scale(1)",
+				transformOrigin: "600px 50%",
+				opacity: 1,
+			},
+			{
+				offset: 1,
+				transform: "translateX(1000px) rotateY(20deg) scale(6)",
+				transformOrigin: "-100% 50%",
+				opacity: 0,
+			},
+		],
+		{
+			duration: 1000,
+			easing: "linear",
+			delay: 0,
+			iterations: 1,
+			direction: "normal",
+			fill: "none",
+		}
+	);
+	setTimeout(() => {
+		container.style.visibility = "hidden";
+	}, 1000);
+}
 
+function slideOutEllipticRightBck(container) {
+	container.animate(
+		[
+			{
+				offset: 0,
+				transform: "translateX(0) rotateY(0) scale(1)",
+				transformOrigin: "-1800px 50%",
+				opacity: 1,
+			},
+			{
+				offset: 1,
+				transform: "translateX(1000px) rotateY(-30deg) scale(0)",
+				transformOrigin: "-100% 50%",
+				opacity: 1,
+			},
+		],
+		{
+			duration: 1000,
+			easing: "linear",
+			delay: 0,
+			iterations: 1,
+			direction: "normal",
+			fill: "none",
+		}
+	);
+	setTimeout(() => {
+		container.style.visibility = "hidden";
+	}, 1000);
+}
+
+// Global method to
 function spawnAnimation(thingToAnimate, mouseEvent, animation) {
-	thingToAnimate.addEventListener(mouseEvent, animation);
+	thingToAnimate.addEventListener(mouseEvent, () => {
+		animation(thingToAnimate);
+	});
 }
--- js/recent-view.js
+++ js/recent-view.js
@@ -2,36 +2,8 @@
 
 console.log("container here");
 const container = document.getElementsByClassName("container")[0];
-
-container.addEventListener("mouseover", () => {
-	container.animate(
-		[
-			{
-				offset: 0,
-				transform: "translateX(0) rotateY(0) scale(1)",
-				transformOrigin: "600px 50%",
-				opacity: 1,
-			},
-			{
-				offset: 1,
-				transform: "translateX(1000px) rotateY(20deg) scale(6)",
-				transformOrigin: "-100% 50%",
-				opacity: 0,
-			},
-		],
-		{
-			duration: 1000,
-			easing: "linear",
-			delay: 0,
-			iterations: 1,
-			direction: "normal",
-			fill: "none",
-		}
-	);
-	setTimeout(() => {
-		container.style.visibility = "hidden";
-	}, 1000);
-});
+console.log(container);
+spawnAnimation(container, "mouseover", slideOutEllipticRightFwd);
 
 function ConstructRecentItems() {
 	var doc = document.getElementById("recent");
@@ -59,3 +31,6 @@ function ConstructRecentItems() {
 }
 
 ConstructRecentItems();
+
+const recentContainer = document.getElementsByClassName("recent-container")[0];
+spawnAnimation(recentContainer, "mouseover", slideOutEllipticRightBck);
